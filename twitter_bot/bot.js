f4b3add740a7aceabcfb8e007d28373f548b147c
sidhant-srivastava <sidhant.sriv@gmail.com>
2020-06-09 16:22:25 +0530

    refactor&hotfix

--- twitter_bot/bot.js
+++ twitter_bot/bot.js
@@ -1,27 +1,22 @@
-var Twitter = require('twitter');
-var config = require('./config.js');
-var T = new Twitter(config);
+const Twitter = require("twitter");
+const config = require("./config.js");
+const T = new Twitter(config);
 
 // Set up your search parameters
-const keywords = 'github';
-
-var params = {
+const keywords = "github";
+const tweet_num = 10;
+const params = {
   q: keywords,
-  count: 10,
-  result_type: 'recent',
-  lang: 'en'
-}
+  count: tweet_num,
+  result_type: "recent",
+  lang: "en",
+};
 
 // Initiate your search using the above paramaters
-T.get('search/tweets', params, function(err, data, response) {
-    try {
-        for(let i = 0;i<=99;i++){
-            console.log(data.statuses[i].text);
-    
-        }
-    } catch (error) {
-        
+T.get("search/tweets", params, function (err, data, response) {
+  try {
+    for (let i = 0; i <= tweet_num - 1; i++) {
+      console.log(data.statuses[i].text);
     }
-
-
-})
+  } catch (error) {}
+});
